25ad4b97371e3e0f150787424313de39d0422fbc
Jesper Schaap <jesper2134@hotmail.com>
2018-10-11 11:59:25 +0200

    11/10/2018 les 6 helemaal af

--- les 6/pe6_FInal_assignment.py
+++ les 6/pe6_FInal_assignment.py
@@ -1,13 +1,27 @@
+# def standaardprijs(afstandKM):
+#     if afstandKM <= 50:
+#         return(str(afstandKM*0.80)+'euro')
+#     else:
+#         return(str(afstandKM*0.60+15)+'euro')
+# print(standaardprijs(51))
+
 def standaardprijs(afstandKM):
     if afstandKM <= 50:
-        return(str(afstandKM*0.80)+'euro')
+        return(afstandKM*0.80)
     else:
-        return(str(afstandKM*0.60+15)+'euro')
+        return(afstandKM*0.60+15)
 print(standaardprijs(51))
 
-def ritprijs(leeftijd, weekendrit, afstandKM):
-    if weekendrit== 'true' and leeftijd <12 and leeftijd >=65:
-        return(afstandKM*0,65)
-    if weekendrit== 'true' and leeftijd >=12 and leeftijd <65:
-        return
 
+def ritprijs(leeftijd, weekendrit, afstandKM):
+    standaardtarief= standaardprijs(afstandKM)
+    if weekendrit== True and( leeftijd <12 or leeftijd >=65):
+        return(standaardtarief*0.65)
+    if weekendrit== True and( leeftijd >=12 or leeftijd <65):
+        return(standaardtarief*0.60)
+    if weekendrit== False and( leeftijd <12 or leeftijd >=65):
+        return(standaardtarief*0.70)
+    if weekendrit== False and( leeftijd >=12 or leeftijd <65):
+        return(standaardtarief*1)
+
+print(ritprijs(11,False,51))
